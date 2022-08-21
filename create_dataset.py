4e840fafd050009862062519de6356d979bf2a1e
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-21 18:43:37 -0500

    Using OpenCV to convert to grayscale

--- create_dataset.py
+++ create_dataset.py
@@ -1,5 +1,5 @@
 import os
-from PIL import Image, ImageOps
+import cv2
 
 base_dir = r'C:\Users\masua\Downloads\Cimat\BSR_bsds500\BSR\BSDS500\data'
 source_dir = 'images'
@@ -9,6 +9,5 @@ sets = ['test', 'train', 'val']
 
 for dname in sets:
     for fname in [name for name in os.listdir(os.path.join(base_dir, source_dir, dname)) if name[-3:] == 'jpg']:
-        image = Image.open(os.path.join(base_dir, source_dir, dname, fname))
-        grayscale = ImageOps.grayscale(image)
-        grayscale.save(os.path.join(base_dir, dest_dir, dname, fname))
+        image = cv2.imread(os.path.join(base_dir, source_dir, dname, fname), cv2.IMREAD_GRAYSCALE)
+        cv2.imwrite(os.path.join(base_dir, dest_dir, dname, fname), image)

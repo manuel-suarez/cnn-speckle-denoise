5e55ceeadf121fe6fe4343a562c53d18186a77da
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-21 18:22:35 -0500

    Convert grayscale images with PIL

--- create_dataset.py
+++ create_dataset.py
@@ -1,10 +1,14 @@
 import os
+from PIL import Image, ImageOps
 
 base_dir = r'C:\Users\masua\Downloads\Cimat\BSR_bsds500\BSR\BSDS500\data'
 source_dir = 'images'
-dest_dir = 'images_gray'
+dest_dir = 'grayscale'
 
 sets = ['test', 'train', 'val']
 
 for dname in sets:
-    print(os.listdir(os.path.join(base_dir, source_dir, dname)))
+    for fname in [name for name in os.listdir(os.path.join(base_dir, source_dir, dname)) if name[-3:] == 'jpg']:
+        image = Image.open(os.path.join(base_dir, source_dir, dname, fname))
+        grayscale = ImageOps.grayscale(image)
+        grayscale.save(os.path.join(base_dir, dest_dir, dname, fname))

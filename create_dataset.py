8d5a562f3d9b77fc264f9b3a6dd36ed3f6d41eb8
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-21 18:51:12 -0500

    Add noise

--- create_dataset.py
+++ create_dataset.py
@@ -1,13 +1,21 @@
 import os
 import cv2
+import numpy as np
+from skimage.util import random_noise
 
 base_dir = r'C:\Users\masua\Downloads\Cimat\BSR_bsds500\BSR\BSDS500\data'
 source_dir = 'images'
 dest_dir = 'grayscale'
+noise_dir = 'noise'
 
 sets = ['test', 'train', 'val']
 
 for dname in sets:
     for fname in [name for name in os.listdir(os.path.join(base_dir, source_dir, dname)) if name[-3:] == 'jpg']:
         image = cv2.imread(os.path.join(base_dir, source_dir, dname, fname), cv2.IMREAD_GRAYSCALE)
+        # Grayscale
         cv2.imwrite(os.path.join(base_dir, dest_dir, dname, fname), image)
+        # Noise
+        noise_img = random_noise(image, mode='s&p', amount=0.3)
+        noise_img = np.array(255*noise_img, dtype='uint8')
+        cv2.imwrite(os.path.join(base_dir, noise_dir, dname, fname), noise_img)
